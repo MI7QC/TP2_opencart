1f53a2b7f46736629abea5a4539213bc938efe91
Soushi888 <Sacha.pignot@gmail.com>
2020-06-25 19:50:06 -0400

    update

--- extension/admin/controller/report/topfifteen.php
+++ extension/admin/controller/report/topfifteen.php
@@ -6,11 +6,8 @@ class ControllerReportTopfifteen extends Controller {
 		$this->load->language('report/topfifteen');
 
 		$data['heading_title'] = $this->language->get('heading_title');
-        $data['text_visited_id'] = $this->language->get('text_visited_id');
         $data['text_url'] = $this->language->get('text_url');
         $data['text_title'] = $this->language->get('text_title');
-        $data['text_ip_address'] = $this->language->get('text_ip_address');
-		$data['text_user_id'] = $this->language->get('text_user_id');
 		
 		$this->document->setTitle($this->language->get('heading_title'));
 
@@ -22,12 +19,8 @@ class ControllerReportTopfifteen extends Controller {
 				foreach ($topFifteen as $visited) {
 					//array dans un array
 					$data['topFifteen'][] = array(
-						'visited_id' => $visited['visited_id'],
 						'url' => $visited['url'],
 						'title' => $visited['title'],
-						'date' => $visited['date'],
-						'ip_address' => $visited['ip_address'],
-						'user_id' => $visited['user_id'],
 						'nb_visite' => $visited['nb_visite']
 					);
 				}
--- extension/admin/model/report/topfifteen.php
+++ extension/admin/model/report/topfifteen.php
@@ -2,10 +2,10 @@
 class ModelReportTopfifteen extends Model {
 	public function getTopFifteen() {
 	
-		$query = $this->db->query("SELECT *, count(title) AS nb_visite, title
+		$query = $this->db->query("SELECT `url`, `title`, count(title) AS nb_visite, title
         from " . DB_PREFIX . "visited
        group by url
-       order by count(title) desc
+       order by nb_visite desc
 	   limit 15");
 		return $query->rows;
 	}
