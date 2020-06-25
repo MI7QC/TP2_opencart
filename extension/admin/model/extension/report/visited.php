7defaa8eee6c4064d7e7490b3d874021728b22fc
Soushi888 <Sacha.pignot@gmail.com>
2020-06-25 18:03:24 -0400

    update

--- extension/admin/model/extension/report/visited.php
+++ extension/admin/model/extension/report/visited.php
@@ -5,23 +5,20 @@ class ModelExtensionReportVisited extends Model
     public function createTable()
     {
         $this->db->query(`CREATE TABLE IF NOT EXISTS ${DB_PREFIX}visited ( 
-            `visited_id` int(11) NOT NULL,
-            `url` varchar(150) DEFAULT NULL,
-            `title` varchar(35) DEFAULT NULL,
-            `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-            `ip_address` varchar(15) DEFAULT NULL,
-            `user_id` int(11) DEFAULT NULL
+            'visited_id' int(11) NOT NULL,
+            'url' varchar(150) DEFAULT NULL,
+            'title' varchar(35) DEFAULT NULL,
+            'date' timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+            'ip_address' varchar(15) DEFAULT NULL,
+            'user_id' int(11) DEFAULT NULL,
+            PRIMARY KEY (store_id))
         ) ENGINE=MyISAM DEFAULT CHARSET=utf8;`);
-
-        $this->db->query("ALTER TABLE `${DB_PREFIX}`
-            ADD PRIMARY KEY (`visited_id`);");
     }
 
 
     //Desinstalation de la table visited
     public function dropTable()
     {
-        $this->db->query(`IF OBJECT_ID('${DB_NAME}.${DB_PREFIX}visited, 'U') IS NOT NULL
-        DROP TABLE ${DB_NAME}.${DB_PREFIX}visited`);
+        $this->db->query(`DROP TABLE ${DB_NAME}.${DB_PREFIX}visited`);
     }
 }
