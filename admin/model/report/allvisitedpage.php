f97f2bb6de677cce75d0d7317292b0a0a056e258
Soushi888 <Sacha.pignot@gmail.com>
2020-06-25 21:17:39 -0400

    update final + cleanage

--- admin/model/report/allvisitedpage.php
+++
@@ -1,9 +0,0 @@
-<?php
-class ModelReportAllvisitedpage extends Model {
-	public function getAllVisitedPage() {
-		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "visited");
-
-		return $query->rows;
-	}
-	
-}
