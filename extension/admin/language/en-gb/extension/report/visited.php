45528953c09a1c4ff80d5fb1233f6fbca0235364
Soushi888 <Sacha.pignot@gmail.com>
2020-06-28 23:18:34 -0400

    traduction FR

--- extension/admin/language/en-gb/extension/report/visited.php
+++ extension/admin/language/en-gb/extension/report/visited.php
@@ -1,5 +1,5 @@
 <?php
-$_['heading_title'] = "Visited Page Module";
+$_['heading_title'] = "Visited Pages Module";
 $_['text_extension'] = "Visited";
 $_['text_instructions'] = "Module Instructions";
-$_['text_about'] = 'this store Module will install a new table named "Visited"';
+$_['text_about'] = 'This Module will install a new table named "Visited"';
---
+++ extension/admin/language/fr-fr/extension/report/visited.php
@@ -0,0 +1,5 @@
+<?php
+$_['heading_title'] = "Module Pages Visitées";
+$_['text_extension'] = "Visited";
+$_['text_instructions'] = "Instructions du module";
+$_['text_about'] = 'Ce module va créer une nouvelle table nommée "Visited"';
